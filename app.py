1ffc8c96af7e820b8ffe8e4584aea952563a372f
siddhganesh <siddhganesh09@gmail.com>
2026-07-28 16:12:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -722,4 +722,5 @@ async def home():
     return HTML_CONTENT
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=False)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=False)
